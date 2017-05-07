189df11d82fe22776fd364b9615cd4ad1dc98bf7
Hamza Tahir <htahir111@gmail.com>
2017-05-07 13:34:29 +0200

    Edited date creation and date modified insertion code

--- code/src/main/java/com/partytimeline/core/BaseEntity.java
+++ code/src/main/java/com/partytimeline/core/BaseEntity.java
@@ -1,6 +1,7 @@
 package com.partytimeline.core;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @MappedSuperclass
 public abstract class BaseEntity {
@@ -8,9 +9,9 @@ public abstract class BaseEntity {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private final Long id;
 
-    private Long date_created;
+    private Date date_created;
 
-    private Long date_modified;
+    private Date date_modified;
 
     @Version
     private Long version;
@@ -21,5 +22,17 @@ public abstract class BaseEntity {
 
     public Long getId() {
         return id;
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        Date new_date = new Date();
+        this.date_created = new_date;
+        this.date_created = new_date;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.date_modified = new Date();
     }
 }
