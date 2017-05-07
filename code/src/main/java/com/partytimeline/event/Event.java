2e2424eed19b0b909f1a981d22b5d0be5cf454b0
Hamza Tahir <htahir111@gmail.com>
2017-05-07 13:37:23 +0200

    Completed model for Event

--- code/src/main/java/com/partytimeline/event/Event.java
+++ code/src/main/java/com/partytimeline/event/Event.java
@@ -5,6 +5,7 @@ import com.partytimeline.core.BaseEntity;
 import javax.persistence.Entity;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.Date;
 
 @Entity
 public class Event extends BaseEntity {
@@ -17,13 +18,16 @@ public class Event extends BaseEntity {
     private String description;
 
     @NotNull
-    private Long start_time;
+    private Date start_time;
 
     @NotNull
-    private Long end_time;
+    private Date end_time;
 
-    public Event(String name) {
+    public Event(String name, String description, Date start_time, Date end_time) {
         this.name = name;
+        this.description = description;
+        this.start_time = start_time;
+        this.end_time = end_time;
     }
 
     public String getName() {
@@ -32,5 +36,29 @@ public class Event extends BaseEntity {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getStartTime() {
+        return start_time;
+    }
+
+    public void setStartTime(Date start_time) {
+        this.start_time = start_time;
+    }
+
+    public Date getEndTime() {
+        return end_time;
+    }
+
+    public void setEndTime(Date end_time) {
+        this.end_time = end_time;
     }
 }
