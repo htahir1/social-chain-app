b2b1fbad280761cd0b77c5dc8fed0d488b692a8b
Hamza Tahir <htahir111@gmail.com>
2017-05-07 14:08:31 +0200

    Changed event image repo

--- code/src/main/java/com/partytimeline/event_image/EventImageRepository.java
+++ code/src/main/java/com/partytimeline/event_image/EventImageRepository.java
@@ -1,8 +1,9 @@
 package com.partytimeline.event_image;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@RepositoryRestResource(path = "event_images", collectionResourceRel = "event_images", itemResourceRel = "event_image")
 public interface EventImageRepository extends CrudRepository<EventImage, Long> {
 }
