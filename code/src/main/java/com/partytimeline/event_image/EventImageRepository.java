31a6e3f9be1aa98d759c8cdb22c33ae3932cbd3d
Hamza Tahir <htahir111@gmail.com>
2017-06-04 17:15:09 +0200

    Added findByEventId

--- code/src/main/java/com/partytimeline/event_image/EventImageRepository.java
+++ code/src/main/java/com/partytimeline/event_image/EventImageRepository.java
@@ -5,9 +5,12 @@ import com.partytimeline.user.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @RepositoryRestResource(path = "event_images", collectionResourceRel = "event_images", itemResourceRel = "event_image")
 public interface EventImageRepository extends CrudRepository<EventImage, Long> {
     EventImage findByUserAndEventAndId(@Param("user") User user, @Param("event") Event event, @Param("id") Long id);
+
+    List<EventImage> findByEventId(@Param("event_id") Long event_id);
 }
